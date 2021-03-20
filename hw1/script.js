b7d51693e2d87398febfa61bbe60d2557c50c4f7
Anton Nikonov <antonanton@MacBook-Pro-Anton.local>
2021-03-20 19:54:46 +0200

    add arr sort

---
+++ hw1/script.js
@@ -0,0 +1,68 @@
+// 1.
+const numbers = [ 1, 2, 41, 42, 5 ,33 , 7, 8, 3, 0 ]
+
+const getSmolestNum = (arr) => ( Math.min(...arr) )
+const getLargestNumber = (arr) => ( Math.max(...arr) )
+
+console.log(
+    getSmolestNum(numbers),
+    getLargestNumber(numbers)
+)
+
+
+// 2.
+const bubbleSort = (arr) => {
+    for (let i = 0, endI = arr.length - 1; i < endI; i++) {
+        for (let j = 0, endJ = endI - i; j < endJ; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let swap = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = swap;
+            }
+        }
+    }
+    return arr;
+}
+
+console.log(bubbleSort(numbers))
+
+
+// 3
+const selectionSort = arr => {
+    for (let i = 0, l = arr.length, k = l - 1; i < k; i++) {
+        let indexMin = i;
+        for (let j = i + 1; j < l; j++) {
+            if (arr[indexMin] > arr[j]) {
+                indexMin = j;
+            }
+        }
+        if (indexMin !== i) {
+            [arr[i], arr[indexMin]] = [arr[indexMin], arr[i]];
+        }
+    }
+    return arr;
+};
+
+console.log(selectionSort(numbers))
+
+
+// 4
+const ShellSort = arr => {
+    const l = arr.length;
+    let gap = Math.floor(l / 2);
+    while (gap >= 1) {
+        for (let i = gap; i < l; i++) {
+            const current = arr[i];
+            let j = i;
+            while (j > 0 && arr[j - gap] > current) {
+                arr[j] = arr[j - gap];
+                j -= gap;
+            }
+            arr[j] = current;
+        }
+        gap = Math.floor(gap / 2);
+    }
+    return arr;
+};
+
+console.log(ShellSort(numbers))
